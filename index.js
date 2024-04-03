7c9ef6ec44fd21b7480770589f9b319b984cdd7a
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-04-03 18:43:05 -0500

    added questions

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
+const { placeholderQuestions } = require('./placeholder-questions');
 
 app.get('/api/health', (req, res) => {
     res.send('OK');
+});
+
+app.get('/api/questions', (req, res) => {
+    res.send(placeholderQuestions);
 });
 
 app.listen(PORT, () => {
