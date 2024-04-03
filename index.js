a37fe9488a46f6ed9d45169dcfdff0b5224c0f74
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-04-03 18:34:12 -0500

    basic server scaffolding

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/api/health', (req, res) => {
+    res.send('OK');
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
